c3b8c83321a850a39827a832d907e312544d7b6b
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-06-07 14:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,37 +1,60 @@
 var katzDeli = [];
 
-function takeANumber (currentLine, newName) {
+function takeANumber (currentLine, newNumber) {
   currentLine.push(newName);
-  return 'Welcome,' + ' ' + newName + '.' + ' You are number' + ' '+ currentLine.length +  ' '+'in line.'
+  return 'Welcome,' + ' ' + newName + '.' + ' You are number' + ' '+ currentLine.length + ' ' +'in line.';
   
 }
   
 function nowServing(currentLine){ var nowServing = 0 ;
-  if(!currentLine.length){
-    return 'There is nobody waiting to be served!';
+  if(!currentLine.length){return 'There is nobody waiting to be served!';
   }
-  
+}
+
   var customer = currentLine.shift();
   return 'Currently serving ' + customer + '.';
   
-}
 
-function currentLine(currentLine){
+  function currentLine(currentLine){
+  if(!currentLine.length){
+    return 'The line is currently empty.';
+  }
+  
+    currentLine.forEach(function(customer,index) {
+    customers += index + 1 + '. ' + customer + '';
+    
+    if (index < currentLine.length -1){
+      customers +=', ';}
+
+function currentLine (currentLine){
   if(!currentLine.length){
     return 'The line is currently empty.';
   }
   
   var customers = " ";
-  // Rewrite this part to use a "for" loop instead of "forEach"
-
-  currentLine.forEach(function(customer,index) {
-    customers += index + 1 + '. ' + customer + '';
+  
+  currentLine.foreach(function(customer,index) {
+    customers += index + 1 + ' .' + customer;
     
     if (index < currentLine.length -1){
-      customers +=', ';
-      
+      customers +=',';
+
     }
-  });
+  }
   
+
   return 'The line is currently:' +  customers;
+
 }
+
+function takeANumber(currentLine)    {
+  if(!currentLine.length) {
+    return ' ';
+  }
+  
+  return;
+  
+  }
+
+
+  
